2ca4735c2787ec72c7b2ffb8ca5d3c22744fb6b5
notsag <maxime@gaston.sh>
2019-05-22 21:39:46 +0200

    Added confirmation to questionnaire section

--- yaml_resume/contact.py
+++ yaml_resume/contact.py
@@ -6,28 +6,48 @@ class Location(yaml.YAMLObject):
     """Full address object"""
     yaml_tag = u'Location'
 
-    def __init__(self):
-        self.ask_location()
+    def __init__(self, address, city, zipcode, state="", country=""):
+        self.address = address
+        self.city = city
+        self.zip = zipcode
+        self.state = state
+        self.country = country
 
-    def ask_location(self):
-        self.address = click.prompt("What is your address?")
-        self.city = click.prompt("In what city?")
-        self.zip = click.prompt("Postal/Zip code of that city?")
-        self.state = click.prompt("In what state? (optional)", default="")
-        self.country = click.prompt("In what country? (optional)", default="")
+    def ask_location():
+        """
+        Prompt questions for location informations
+        returns a Location object
+        """
+        address = click.prompt("What is your address?")
+        city = click.prompt("In what city?")
+        zipcode = click.prompt("Postal/Zip code of that city?")
+        state = click.prompt("In what state? (optional)", default="")
+        country = click.prompt("In what country? (optional)", default="")
+        return Location(address, city, zipcode, state, country)
 
 
 class Contact(yaml.YAMLObject):
     """Contact object"""
     yaml_tag = u'Contact'
 
-    def __init__(self):
-        self.ask_contact()
+    def __init__(self, name, job, email, phone, location):
+        self.name = name
+        self.job = job
+        self.email = email
+        self.phone = phone
+        self.location = location
 
-    def ask_contact(self):
-        """Prompt questions for contact informations"""
-        self.name = click.prompt("What is your name?")
-        self.job = click.prompt("What is your job title?")
-        self.email = click.prompt("What is your email address?")
-        self.phone = click.prompt("What is your phone number?")
-        self.location = Location()
+    def ask_contact():
+        """
+        Prompt questions for contact informations
+        returns a Contact object
+        """
+        correct = False
+        while not correct:
+            name = click.prompt("What is your name?")
+            job = click.prompt("What is your job title?")
+            email = click.prompt("What is your email address?")
+            phone = click.prompt("What is your phone number?")
+            location = Location.ask_location()
+            correct = click.confirm("Is this correct?")
+        return Contact(name, job, email, phone, location)
