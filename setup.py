9fa583c300989a1a56b36e1bfc829d23e69856f6
notsag <maxime@gaston.sh>
2019-08-06 17:34:29 +0200

    Fix dependencies in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from os import path
 
-DIR = path.dirname(path.abspath(__file__))
-INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
-
-with open(path.join(DIR, 'README.md')) as f:
+with open('README.md') as f:
     README = f.read()
 
 setup(
@@ -21,7 +17,7 @@ setup(
     author_email='maxime@gaston.sh',
     license='gpl-3.0',
     keywords=['resume', 'yaml'],
-    install_requires=INSTALL_PACKAGES,
+    install_requires=['click', 'pyyaml', 'cerberus'],
     entry_points={
         'console_scripts': [
             'yaml-resume = yaml_resume.cli:cli',
